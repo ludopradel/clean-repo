8e5cfb7d0119eb4940f22641824ff37c3b08e411
ludopradel <ludovic.pradel@gmail.com>
2016-11-16 21:48:38 +0100

    project init

---
+++ src/test/java/katas/AppTest.java
@@ -0,0 +1,12 @@
+package katas;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class AppTest
+{
+	@Test public void 
+	should_do_something() {
+		assertThat(true).isFalse();
+	}
+}
